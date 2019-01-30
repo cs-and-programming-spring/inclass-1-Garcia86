2e1f6d2330743e886106914e0cfcc4140f36f776
garcia86 <garcia86@RobiLab325-25>
2019-01-29 19:35:58 -0500

    Created a circumference calculator for a circle.

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,7 @@
 * Email: garcia86@students.rowan.edu
 ***********************************************
 * Purpose:
-* This Program converts a given distance in miles into kilometers.
+* This program calculates the circumference of a circle.
 ***********************************************/
 
 #include <iostream>
@@ -14,6 +14,22 @@ const  float PI = 3.14159;
 
 int main()
 {
+		float radius, circumference;
+
+	//Display purpose of the program
+	cout << " ******************************************" << endl;
+	cout << " This program calculates the circumference of a circle." << endl;
+	cout << " ******************************************\n\n";
+
+	//Get user input
+	cout << "Please input radius of circle: ";
+	cin >> radius;
+
+	//Compute circumference to 2 * PI * radius
+	circumference = 2 * PI * radius;
+
+	//Display the distance in kilometers 
+	cout << "The circumference of the circle is = " << circumference << "\n\n";
 
 		float miles, kilometers;
 
@@ -30,10 +46,9 @@ int main()
 	kilometers = miles * 1.60934;
 
 	//Display the distance in kilometers 
-	cout << " The distance in kilometers is = " << kilometers << "\n\n";
-	cout << "Program has ended" << endl;
+	cout << "The distance in kilometers is = " << kilometers << "\n\n";
 
-	float radius, area, circumference;
+		float area;
 	
 	//Display purpose of the program
 	cout << " ******************************************"<<endl;
